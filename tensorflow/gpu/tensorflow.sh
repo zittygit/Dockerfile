d4e4f729d7a1ed450da161f67b952dd935044812
zhangzy <zhang061872@qq.com>
2018-09-27 14:27:34 +0800

    zitty

--- tensorflow/gpu/tensorflow.sh
+++ tensorflow/gpu/tensorflow.sh
@@ -4,6 +4,6 @@ cd /notebooks
 export HOME=/tmp
 export TERM=xterm
 mkdir /tmp/log
-gotty --port 8000 --permit-write --reconnect /bin/bash &
+/bin/gotty --port 8000 --permit-write --reconnect /bin/bash &
 tensorboard --port 8888 --logdir /tmp/log &
 jupyter notebook --notebook-dir /notebooks --config /config.json --allow-root 
